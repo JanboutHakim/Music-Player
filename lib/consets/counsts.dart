8a5cfbd4143c48bfbc369e1ad0a6e5a02c1c9e29
Janbout <Kawthery9@gmail.com>
2023-08-13 15:44:34 +0300

    initialz varibles and fonts

---
+++ lib/consets/counsts.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+//Colors define
+var darkmodebackground=const Color.fromRGBO(9,18,39,1);
+ var normalmodebackground=const Color.fromRGBO(234,240,255,1);
+
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+import 'package:music_player/pages/Home.dart';
+
+void main() {
+  runApp(MaterialApp(routes: {
+    '/': (context)=> Home(),
+  },));
+}
